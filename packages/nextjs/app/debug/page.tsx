b04692c505916a7bfdacc45823e08140b13ab023
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2024-02-15 21:14:50 +0500

    Update page.tsx

--- packages/nextjs/app/debug/page.tsx
+++ packages/nextjs/app/debug/page.tsx
@@ -5,7 +5,7 @@ import type { NextPage } from "next";
 import { useLocalStorage } from "usehooks-ts";
 import { BarsArrowUpIcon } from "@heroicons/react/20/solid";
 import { MetaHeader } from "~~/components/MetaHeader";
-import { ContractUI } from "~~/_components/contract";
+import { ContractUI } from "~~/utils/scaffold-eth";
 import { ContractName } from "~~/utils/scaffold-eth/contract";
 import { getAllContracts } from "~~/utils/scaffold-eth/contractsData";
 
