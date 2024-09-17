c953514ab27ebc76bb75c5711e2e3037c41d4f78
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2024-09-17 22:34:08 +0500

    Update page.tsx

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -5,7 +5,7 @@ import type { NextPage } from "next";
 //import { useAccount } from "wagmi";
 //import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 //import { Address } from "~~/components/scaffold-eth";
-import {MetaHeader} from  "~~/components/MetaHeader.tsx";
+import {MetaHeader} from  "~~/components";
 
 
 const Home: NextPage = () => {
